97005bab877584cd7de9c2137f99422e89d8d704
juon <juonjang@gmail.com>
2023-10-24 11:26:22 +0700

    addFooter

--- src/app/(home)/ui/HomePage.tsx
+++ src/app/(home)/ui/HomePage.tsx
@@ -4,12 +4,14 @@ import React from 'react'
 import { Button,Title } from '@mantine/core'
 import { HeroImageBackground } from './HeroImageBackground'
 import { AppFeatures } from './AppFeature'
+import { FooterSocial } from './FooterSocial'
 
 export default function HomeContent() {
   return (
     <div>
         <HeroImageBackground/>
         <AppFeatures  />
+        <FooterSocial/>
     </div>
   )
 }
